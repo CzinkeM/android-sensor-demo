bd1e1051316878110f52a05cf6f9edad35992598
CzinkeM <mczinke@protonmail.com>
2022-10-24 18:10:39 +0200

    Added abstraction to sensor logic.

---
+++ app/src/main/java/hu/mczinke/sensor_demo/MeasurableSensor.kt
@@ -0,0 +1,17 @@
+package hu.mczinke.sensor_demo
+
+import android.hardware.Sensor
+
+abstract class MeasurableSensor(protected val sensorType: Int) {
+
+    protected var onSensorValueChanged: ((List<Float>) -> Unit)? = null
+
+    abstract val doesSensorExist: Boolean
+
+    abstract fun startListening()
+    abstract fun stopListening()
+
+    fun setOnSensorValuesChangedListener(listener: ((List<Float>)-> Unit)) {
+        onSensorValueChanged = listener
+    }
+}
