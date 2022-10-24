a848681495c8c0d662398df54a3c4fd759294ddc
CzinkeM <mczinke@protonmail.com>
2022-10-24 18:10:53 +0200

    Created sensors.

---
+++ app/src/main/java/hu/mczinke/sensor_demo/Sensors.kt
@@ -0,0 +1,37 @@
+package hu.mczinke.sensor_demo
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+
+class LightSensor(
+    context: Context
+): AndroidSensor(
+    context = context,
+    sensorFeature = PackageManager.FEATURE_SENSOR_LIGHT,
+    sensorType = Sensor.TYPE_LIGHT
+)
+
+class GyroscopeSensor(
+    context: Context
+): AndroidSensor(
+    context = context,
+    sensorFeature = PackageManager.FEATURE_SENSOR_GYROSCOPE,
+    sensorType = Sensor.TYPE_GYROSCOPE
+)
+
+class AccelerometersSensor(
+    context: Context
+): AndroidSensor(
+    context = context,
+    sensorFeature = PackageManager.FEATURE_SENSOR_ACCELEROMETER,
+    sensorType = Sensor.TYPE_ACCELEROMETER
+)
+
+class ProximitySensor(
+    context: Context
+): AndroidSensor(
+    context = context,
+    sensorFeature = PackageManager.FEATURE_SENSOR_PROXIMITY,
+    sensorType = Sensor.TYPE_PROXIMITY
+)
